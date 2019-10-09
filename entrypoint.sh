40e7bf5422257cc3f6b38477bc64d95182fb3f68
sakebook <sakebook@gmail.com>
2019-10-10 01:53:50 +0900

    Debugging3

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ echo "Hi! $1"
 
 echo "GREET: $GREET"
 echo "who to: $INPUT_WHO_TO_GREET"
-echo "greet: $(who-to-greet)"
+echo "greet: $who-to-greet"
 
 time=$(date)
 echo ::set-output name=time::$time
