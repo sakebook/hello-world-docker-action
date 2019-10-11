9b2a910ce78abb0bb2fad0d673fb6d7f388b9f90
sakebook <sakebook@gmail.com>
2019-10-11 23:09:06 +0900

    Test JSON

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,9 +2,17 @@
 
 echo "Hi! $1"
 
+create_json() {
+  cat <<EOF > sample.json
+$INPUT_JSON
+EOF
+}
+
 echo "GREET: $GREET"
 echo "who to: $INPUT_WHO_TO_GREET"
 echo "greet: $who_to_greet"
+create_json
+cat sample.json
 
 time=$(date)
 echo ::set-output name=time::$time
